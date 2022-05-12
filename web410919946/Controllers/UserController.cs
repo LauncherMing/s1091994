41a7c48402c15a807077f43fa05285b4e6d7f16d
黃明聖 <s1091994@gm.pu.edu.tw>
2022-05-12 20:59:32 +0800

    新增User Controllers/UserController.cs 跟 SignUp.cshtml

---
+++ web410919946/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web410919946.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(String name,String account,String password)
+        {//IsNullOrWhiteSpace() need using Microsoft.Ajax.Utilities
+            if (name.IsNullOrWhiteSpace())//if (name == null || name == "")
+            {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            if (account.IsNullOrWhiteSpace())//if (account == null || account == "")
+            {
+                ViewBag.AccountMessage = "請輸入帳號";
+            }
+            if (password.IsNullOrWhiteSpace())//if (password == null || password == "")
+            {
+                ViewBag.PasswordMessage = "請輸入密碼";
+            }
+            if (!name.IsNullOrWhiteSpace() && !account.IsNullOrWhiteSpace() && !password.IsNullOrWhiteSpace())
+            //if (!(name == null || name == "") && !(account == null || account == "") && !(password == null || password == ""))
+            {
+                ViewBag.Message = "註冊成功";
+            }
+            return View();
+        }
+    }
+}
